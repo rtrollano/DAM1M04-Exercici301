6dab08dba6f48b1a14ba835396e0a39210773631
rtrollano <r.trollano.l@gmail.com>
2026-02-13 19:33:13 +0100

    Subida final

--- server/app.js
+++ server/app.js
@@ -23,7 +23,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 // Registrar "Helpers .hbs" aquí
-hbs.registerHelper('lt', (a, b) => a < b);
+hbs.registerHelper('lte', (a, b) => a <= b);
 
 // Partials de Handlebars
 hbs.registerPartials(path.join(__dirname, 'views', 'partials'));
